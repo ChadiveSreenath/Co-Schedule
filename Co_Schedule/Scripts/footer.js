63affb4597b46be10a68d91ec8c67260b86be916
Chadive Sreenath <100198623+ChadiveSreenath@users.noreply.github.com>
2022-05-02 20:28:25 +0530

    my first commit

---
+++ Co_Schedule/Scripts/footer.js
@@ -0,0 +1,116 @@
+
+
+function footer() {
+
+    return `
+    <div id="footer">
+
+    <div id="inner-container">
+
+        <div id="footer-start">
+
+            <img src="https://coschedule.com/img/cos-logo-icon.svg" alt="" id="logo">
+            <a href="Signin.html">
+            <p id="footer-p">Sign Up</p>
+            </a>
+            <a href="requestDemo.html">
+            <p id="footer-p">Request A Demo</p>
+            </a>
+            <a href="Signup.html">
+            <p id="footer-p">Sign In</p>
+            </a>
+
+        </div>
+
+        <div id="products">
+            <h5>PRODUCTS</h5>
+            <span>
+                <img src="https://coschedule.com/img/product/coschedule-marketing-calendar-product-icon.svg" alt="" id="icon">
+                <p id="footer-p">Marketing Calender</p>
+            </span>
+            <span>
+                <img src="https://coschedule.com/img/product/coschedule-suite-product-icon.svg" alt="" id="icon">
+                <p id="footer-p">Marketing Suite</p>
+            </span>
+            <span>
+                <img src="https://coschedule.com/img/headline-studio/hs-icon-blue.svg" alt="" id="icon">
+                <p id="footer-p">Headline Studio</p> 
+            </span>
+            <span>
+                <img src="https://coschedule.com/img/academy/ami-logo-color.svg" alt="" id="icon">
+                <p id="footer-p">Actionable Marketing Institute</p>
+            </span>
+
+        </div>
+
+
+        <div id="whycoschedule">
+            
+            <h5>WHY COSCHEDULE</h5>
+            <P id="footer-p">Customer Stories</P>
+            <p id="footer-p">Support</p>
+            <p id="footer-p">Product Announcements</p>
+            <p id="footer-p">Pricing</p>
+        </div>
+
+
+        <div id="company">
+            <h5>COMPANY</h5>
+            <p id="footer-p">About</p>
+            <p id="footer-p">Jobs </p>
+            <p id="footer-p">Press</p>
+            <p id="footer-p">Contact Us</p>
+            <p id="footer-p">Affiliate Program</p>
+            <p id="footer-p">Terms & Policies</p>
+        </div>
+
+
+        <div id="resources">
+            
+            <h5>RESOURCES</h5>
+            <p id="footer-p">CoSchedule Marketing Blog</p>
+            <p id="footer-p">Actionable Marketing Podcast</p>
+            <p id="footer-p">10x Marketing Formula Book</p>
+            <p id="footer-p">Get Started With Agile Marketing Book</p>
+            <p id="footer-p">Marketing Ideas Hub</p>
+        </div>
+
+
+        <div id="guides">
+            <h5>GUIDES</h5>
+            <p id="footer-p">CoSchedule Guide</p>
+            <p id="footer-p">Agile Marketing Guide</p>
+            <p id="footer-p">Marketing Strategy Guide</p>
+            <p id="footer-p">Content Marketing Guide</p>
+        </div>
+
+
+        <div id="tools">
+            <h5>TOOLS</h5>
+            <p id="footer-p">Headline Analyzer</p>
+            <p id="footer-p">Social Message Optimizer</p>
+            <p id="footer-p">Email Subject Line Tester</p>
+            <p id="footer-p">Marketing Dictionary</p>
+        </div>
+
+
+        <div id="footer-end">
+            <p id="footer-p">© Copyright 2022, all rights reserved. See our Terms & Policies.</p>
+            <span>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-pinterest"></i>
+                <i class="fa-brands fa-instagram"></i>
+                
+            </span>
+
+        </div>
+
+    </div>
+
+</div>
+    `
+}
+
+export {footer} ;
---
+++ Co_Schedule/Scripts/navbar.js
@@ -0,0 +1,28 @@
+
+
+function navbar(){
+
+    return `
+    
+    <div id="navbar">
+    <div id="left-bar">
+      <img src="https://coschedule.com/img/cos-logo-full-color.svg" alt="" id="navbar-logo">
+      <h5 id="navbar-head">Products</h5>
+      <h5 id="navbar-head">Why Coschedule</h5>
+      <h5 id="navbar-head">Resources</h5>
+      <h5 id="navbar-head">Pricing</h5>
+    </div>
+  <div id="right-bar">
+       <a href="Signin.html">
+        <h5 id="navbar-head">Sign In</h5>
+      </a>
+      <h5 id="navbar-head">Get Started Free</h5> 
+      <button id="navbar-btn">Get A Demo</button>
+  </div>
+</div>
+    
+    `
+
+}
+
+export {navbar}
