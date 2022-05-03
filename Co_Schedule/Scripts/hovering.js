7197b94c98e5acb0b60225599e2d23aaee43ea62
Chadive Sreenath <100198623+ChadiveSreenath@users.noreply.github.com>
2022-05-03 14:41:21 +0530

    updated and created new pages

---
+++ Co_Schedule/Scripts/hovering.js
@@ -0,0 +1,49 @@
+
+
+
+const products =  () => {
+    
+    return `
+    <div id="navbar-products-hover">
+    <div id="content-calenders-hover">
+        <div id="title">CONTENT CALENDERS</div>
+        <div id="marketing-calender-hover">
+            <img  id="icon" src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-calendar-product-icon.png&w=64&q=75" alt="">
+            <div id="inside-hover">
+            <div id="hover-heading">Marketing Calendar</div>
+            <p id="hover-para">A calendar that helps you see, schedule, and share your&nbsp;marketing.</p>
+            </div>
+        </div>
+
+        <div id="marketing-suite-hover"> 
+            <img id="icon" src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-suite-product-icon.png&w=64&q=75" alt="">
+            <div id="inside-hover">
+                <div id="hover-heading">Marketing Suite</div>
+                <p id="hover-para">A family of agile marketing products to coordinate your team, projects, and&nbsp;process.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div id="content-optimisation-hover">
+        <div id="title">CONTENT OPTIMISATION</div>
+
+        <div id="inside-optimisation-hover">
+            <img  src="https://coschedule.com/img/headline-studio/hs-icon-blue.svg" alt="" id="icon-small">
+            <h5>Headline Studio</h5>
+        </div>
+
+        <div id="title">MARKETING EDUCATION</div>
+
+        <div id="inside-optimisation-hover">
+            <img src="https://coschedule.com/img/academy/ami-logo-color.svg" alt="" id="icon-small">
+            <h5>Actionable Marketing Institute</h5>
+        </div>
+
+    </div>
+</div>
+    `
+}
+
+export {products}
+
